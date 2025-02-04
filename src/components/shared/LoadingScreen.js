b0ac55e5a8e9aade007468b30eca144bcca68767
Timm Schoenborn <timmschoenborn@gmail.com>
2025-02-04 18:57:33 -0500

    Updates

--- src/components/shared/LoadingScreen.js
+++
@@ -1,9 +0,0 @@
-import { Spinner } from "react-bootstrap";
-
-const LoadingScreen = () => (
-  <div className="container-sm" style={{ textAlign: "center" }}>
-    <Spinner role="status" animation="border" variant="warning" />
-  </div>
-);
-
-export default LoadingScreen;
